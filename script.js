803c1bbce412dac282e216755eecb7b013b2d4cd
Egor <vegormak7@gmail.com>
2022-11-20 12:51:07 +0300

    fix zoom 2chance

--- script.js
+++ script.js
@@ -346,14 +346,32 @@ document.onkeydown = function(e) {
     draw();
 };
 
-var lastTouchEnd = 0;
-document.addEventListener('touchend', function (event) {
-    var now = (new Date()).getTime();
-    if (now - lastTouchEnd <= 300) {
-        event.preventDefault();
+let drags = new Set(); //set of all active drags
+document.addEventListener("touchmove", function(event){
+  if(!event.isTrusted){return;}//don't react to fake touches
+  Array.from(event.changedTouches).forEach(function(touch){
+    drags.add(touch.identifier); //mark this touch as a drag
+  });
+});
+document.addEventListener("touchend", function(event){
+  if(!event.isTrusted){return;}
+  let isDrag = false;
+  Array.from(event.changedTouches).forEach(function(touch){
+    if(drags.has(touch.identifier)){
+      isDrag = true;
     }
-    lastTouchEnd = now;
-}, false);
+    drags.delete(touch.identifier);//touch ended, so delete it
+  });
+  if(!isDrag && document.activeElement == document.body){
+    //note that double-tap only happens when the body is active
+    event.preventDefault(); //don't zoom
+    event.stopPropagation(); //don't relay event
+    event.target.focus(); //in case it's an input element
+    event.target.click(); //in case it has a click handler
+    event.target.dispatchEvent(new TouchEvent("touchend",event));
+    //dispatch a copy of this event (for other touch handlers)
+  }
+});
 
 
 // mobile events
