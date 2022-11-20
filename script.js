0adc6dcf34fc4018fe896330aa8e2f37a17d0038
Egor <vegormak7@gmail.com>
2022-11-20 13:15:26 +0300

    ......

--- script.js
+++ script.js
@@ -350,17 +350,9 @@ let preLastTouchStartAt = 0;
 let lastTouchStartAt = 0;
 const delay = 500;
 
-document.addEventListener('touchstart', () => {
-  preLastTouchStartAt = lastTouchStartAt;
-  lastTouchStartAt = +new Date();
-});
-document.addEventListener('touchend', (event) => {
-  const touchEndAt = +new Date();
-  if (touchEndAt - preLastTouchStartAt < delay) {
-    event.preventDefault();
-    event.target.click();
-  }
-});
+document.ondblclick = function(e) {
+    e.preventDefault();
+};
 
 // mobile events
 let startPoint={};
