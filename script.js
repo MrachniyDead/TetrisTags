df02f2ccdf00457c283888bcae7d8a94143e7c03
Egor <vegormak7@gmail.com>
2022-11-20 13:10:46 +0300

    ....

--- script.js
+++ script.js
@@ -346,6 +346,21 @@ document.onkeydown = function(e) {
     draw();
 };
 
+let preLastTouchStartAt = 0;
+let lastTouchStartAt = 0;
+const delay = 500;
+
+document.addEventListener('touchstart', () => {
+  preLastTouchStartAt = lastTouchStartAt;
+  lastTouchStartAt = +new Date();
+});
+document.addEventListener('touchend', (event) => {
+  const touchEndAt = +new Date();
+  if (touchEndAt - preLastTouchStartAt < delay) {
+    event.preventDefault();
+    event.target.click();
+  }
+});
 
 // mobile events
 let startPoint={};
