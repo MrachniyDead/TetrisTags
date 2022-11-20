b1141bab14de68c2adcd1c30166f3cca191c209c
Egor <vegormak7@gmail.com>
2022-11-20 13:04:09 +0300

    ...

--- script.js
+++ script.js
@@ -346,20 +346,6 @@ document.onkeydown = function(e) {
     draw();
 };
 
-function preventZoom(e) {
-    var t2 = e.timeStamp;
-    var t1 = e.currentTarget.dataset.lastTouch || t2;
-    var dt = t2 - t1;
-    var fingers = e.touches.length;
-    e.currentTarget.dataset.lastTouch = t2;
-  
-    if (!dt || dt > 500 || fingers > 1) {return;} // not double-tap
-  
-    e.preventDefault();
-    e.target.click();
-}
-
-document.addEventListener('touchstart', preventZoom); 
 
 // mobile events
 let startPoint={};
